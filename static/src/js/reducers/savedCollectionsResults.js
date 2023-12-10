dab2c0cc7874a896b90952b955c9e76b91096670
Ed Olivares <34591886+eudoroolivares2016@users.noreply.github.com>
2023-12-10 14:02:11 -0500

    Comment out unused modual

--- static/src/js/reducers/savedCollectionsResults.js
+++ static/src/js/reducers/savedCollectionsResults.js
@@ -1,70 +1,70 @@
-import camelCaseKeys from 'camelcase-keys'
+// Import camelCaseKeys from 'camelcase-keys'
 
-import {
-  DELETE_SAVED_COLLECTIONS_RESULTS,
-  UPDATE_SAVED_COLLECTIONS_RESULTS
-} from '../constants/actionTypes'
+// import {
+//   DELETE_SAVED_COLLECTIONS_RESULTS,
+//   UPDATE_SAVED_COLLECTIONS_RESULTS
+// } from '../constants/actionTypes'
 
-const initialState = {}
+// const initialState = {}
 
-/**
- * Transforms CMR metadata before adding to the store
- * @param {Array} results Array of metadata results from CMR
- */
-const processResults = (results) => {
-  const byId = {}
+// /**
+//  * Transforms CMR metadata before adding to the store
+//  * @param {Array} results Array of metadata results from CMR
+//  */
+// const processResults = (results) => {
+//   const byId = {}
 
-  results.forEach((result) => {
-    const { id } = result
+//   results.forEach((result) => {
+//     const { id } = result
 
-    byId[id] = {
-      ...camelCaseKeys(result, {
-        exclude: [
-          'isCSDA'
-        ]
-      }),
-      conceptId: id
-    }
-  })
+//     byId[id] = {
+//       ...camelCaseKeys(result, {
+//         exclude: [
+//           'isCSDA'
+//         ]
+//       }),
+//       conceptId: id
+//     }
+//   })
 
-  return byId
-}
+//   return byId
+// }
 
-const savedCollectionsResultsReducer = (state = initialState, action = {}) => {
-  switch (action.type) {
-    case UPDATE_SAVED_COLLECTIONS_RESULTS: {
-      console.log('update saved collection metadata func.')
-      const newState = {}
+// const savedCollectionsResultsReducer = (state = initialState, action = {}) => {
+//   switch (action.type) {
+//     case UPDATE_SAVED_COLLECTIONS_RESULTS: {
+//       console.log('update saved collection metadata func.')
+//       const newState = {}
 
-      console.log(action.payload)
-      const processPayload = processResults(action.payload.results)
+//       console.log(action.payload)
+//       const processPayload = processResults(action.payload.results)
 
-      const currentKeys = Object.keys(processPayload)
-      console.log(processPayload)
-      currentKeys.forEach((collectionId) => {
-        const { [collectionId]: currentState = {} } = state
+//       const currentKeys = Object.keys(processPayload)
+//       console.log(processPayload)
+//       currentKeys.forEach((collectionId) => {
+//         const { [collectionId]: currentState = {} } = state
 
-        newState[collectionId] = {
-          ...currentState,
-          ...processPayload[collectionId]
-        }
-      })
+//         newState[collectionId] = {
+//           ...currentState,
+//           ...processPayload[collectionId]
+//         }
+//       })
 
-      return {
-        ...state,
-        ...newState
-      }
-    }
+//       return {
+//         ...state,
+//         ...newState
+//       }
+//     }
 
-    case DELETE_SAVED_COLLECTIONS_RESULTS: {
-      console.log('delete saved collections')
+//     case DELETE_SAVED_COLLECTIONS_RESULTS: {
+//       console.log('delete saved collections')
 
-      return {}
-    }
+//       return {}
+//     }
 
-    default:
-      return state
-  }
-}
+//     default:
+//       return state
+//   }
+// }
 
-export default savedCollectionsResultsReducer
+// export default savedCollectionsResultsReducer
